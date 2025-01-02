b3d4240ea5f462e1ddf3c8c1791cc0833c344bc9
Ankit <83949374+devankit57@users.noreply.github.com>
2025-01-02 22:44:52 +0530

    Add files via upload

--- Web App/src/lib/mongodb.ts
+++ Web App/src/lib/mongodb.ts
@@ -18,7 +18,7 @@ let clientPromise: Promise<MongoClient>
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  let globalWithMongo = global as typeof globalThis & {
+  const globalWithMongo = global as typeof globalThis & {
     _mongoClientPromise?: Promise<MongoClient>
   }
 
@@ -34,4 +34,3 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 export default clientPromise
-
