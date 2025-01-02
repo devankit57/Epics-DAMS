cc4c11d4144bd44d37d25e77615de24fc4dcc833
Ankit <83949374+devankit57@users.noreply.github.com>
2025-01-02 22:55:14 +0530

    Update route.ts

--- Web App/src/app/api/auth/logout/route.ts
+++ Web App/src/app/api/auth/logout/route.ts
@@ -2,7 +2,8 @@ import { NextResponse } from 'next/server'
 import { cookies } from 'next/headers'
 
 export async function POST() {
-  cookies().delete('auth-token')
+  const cookieStore = cookies()
+  cookieStore.set('auth-token', '', { expires: new Date(0) }) // Set the cookie with an expired date to delete it.
+  
   return NextResponse.redirect(new URL('/login', process.env.NEXT_PUBLIC_BASE_URL))
 }
-
