16bf283db6b990885458d02e34fd3b382453ac64
Ankit <83949374+devankit57@users.noreply.github.com>
2025-01-02 22:30:57 +0530

    Add files via upload

--- Web App/eslint.config.mjs
+++ Web App/eslint.config.mjs
@@ -11,6 +11,15 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Disable specific ESLint rules
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off", // Disable the rule for using `any` type
+      "no-console": "off", // Disable no-console rule (if needed)
+      // Add other rules to disable here
+    },
+  },
 ];
 
 export default eslintConfig;
