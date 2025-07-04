99a30a3c66d43e00440a7f0e6f6db9cc5d2a93a5
Ankit <83949374+devankit57@users.noreply.github.com>
2025-07-04 21:54:07 +0530

    Delete Web App directory

--- Web App/src/app/api/auth/logout/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-import { cookies } from 'next/headers'
-
-export async function POST() {
-  const cookieStore = await cookies() // Await the promise
-  cookieStore.set('auth-token', '', { expires: new Date(0) }) // Set the cookie with an expired date to delete it.
-
-  return NextResponse.redirect(new URL('/login', process.env.NEXT_PUBLIC_BASE_URL))
-}
--- Web App/src/app/components/ui/badge.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const Badge = ({ children, className }: { children: React.ReactNode; className?: string }) => (
-  <span className={`inline-block px-3 py-1 rounded-md text-sm ${className}`}>{children}</span>
-);
